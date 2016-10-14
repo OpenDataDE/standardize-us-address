a5dcf69af43e5acd216e0dcd8db44e79e2ba7af8
Chris Alan Williams <chris@enactdevelopment.com>
2016-10-14 14:15:38 -0400

    initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+
+Basic function that uses the usaddress library by DataMade to tokenize an address, then standardizes it as much as possible
+
+
+https://github.com/datamade/usaddress
+
+"""
+
+from standardize_us_address.functions import standardize_us_address
